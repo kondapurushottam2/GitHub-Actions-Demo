8ee7ad377ca7063968ccac67316eecd1927b90fd
Purushottam <kondapurushottam2@gmail.com>
2023-01-29 12:51:03 +0530

    updated changes in adding program file

--- Simple_Programs/AddToNumbers.py
+++ Simple_Programs/AddToNumbers.py
@@ -5,6 +5,6 @@ num2 = 6.3
 
 # Add two numbers
 sum = num1 + num2
-
+print("this is the changes made for first time")
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
